2e90a980d59087d2f70db117162b57f39445e2f0
Furkan Polat <furkanpolat2413@gmail.com>
2024-04-07 20:28:00 +0300

    Giriş ekranı
    Giriş ekranının büyük bölümü bitti fakat birkaç eksik var.

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Kereme boydan</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>   
+
+      <View style={{width:"85%",height:"10%", justifyContent:"flex-end",}}>
+        <Text style={styles.bandil}>BANDIL APP</Text>
+      </View>
+
+      <View style={{width:"85%",height:"10%",justifyContent:"center"}}>
+        <TextInput placeholder='Kullanıcı Adınız' placeholderTextColor={"gray"} style={styles.textinput}></TextInput>
+      </View>
+
+      <View style={{width:"85%",height:"10%", justifyContent:"center"}}>
+        <TextInput placeholder='Şifreniz' placeholderTextColor={"gray"} style={styles.textinput}></TextInput>
+      </View>
+        
+      <View style={{width:"60%", height:"5%", borderRadius:50, backgroundColor:"#6CA4E0", justifyContent:"center", marginBottom:10}}>
+        <Text style={{color:"white", textAlign:"center",}}>Giriş Yap</Text>
+      </View>
+        
+      <View style={{width:"85%", height:"10%", flexDirection:"row"}}>
+
+        <View style={{flex:2, alignItems:"flex-end"}}>
+        <Text style={{color:"white", fontSize:15 }}>       Yeni misiniz ?  O halde  </Text>
+        </View>
+
+        <View style={{flex:1}}>
+          <Text style={{color:"#3616D5",  fontSize:15 }}>Kayıt Ol</Text>
+        </View>
+
+      </View>
+      <StatusBar style="light" />   
     </View>
+    
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bandil:{
+    fontWeight: "bold",
+    textAlign:"center",
+    fontSize:35,
+    color:"white",
+  },
+  textinput:{
+    borderWidth:2,
+    width:'100%',
+    height:'75%',
+    borderRadius:40,
+    textAlign:"center",
+    borderColor:"white",
+    
   },
 });
